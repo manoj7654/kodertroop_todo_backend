32283fd8ab31d7aa37a737d803d400a68d5e5efe
Manoj Kumar <manojsfstm5@gmail.com>
2023-06-29 16:40:37 +0530

    route protected

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const { addTask, getTask, searchTask, deletTask, updateTask } = require("../controller/todo.controller")
+const { addTask, getTask, redis_post, searchTask, deletTask, updateTask } = require("../controller/todo.controller")
 const { authenticate } = require("../middleware/authenticate")
 const todoRouter=express.Router()
 
@@ -10,57 +10,20 @@ todoRouter.get("/todo",(req,res)=>{
     res.send("Hello from Todo App")
 })
 
-
+// for add todo
 todoRouter.post("/add",authenticate,addTask)
+
+// for getting all todo
 todoRouter.get("/allTodo",getTask)
+
+// for searching todo
 todoRouter.get("/search",searchTask)
-todoRouter.delete("/remove/:id",deletTask)
-todoRouter.patch("/edit/:id",updateTask)
-// todoRouter.get("/search",searchTaskbyDesc)
+
+// delete todo
+todoRouter.delete("/remove/:id",authenticate,deletTask)
+
+// update todo
+todoRouter.patch("/edit/:id",authenticate,updateTask)
+
 module.exports={todoRouter}
 
-/* exports.getTasks = async (req, res) => {
-    try {
-      req.redisClient.get('tasks', async (error, cachedTasks) => {
-        if (error) {
-          console.error('Error fetching tasks from Redis:', error);
-          return res.status(500).json({ error: 'An error occurred' });
-        }
-  
-        if (cachedTasks) {
-          const tasks = JSON.parse(cachedTasks);
-          res.json(tasks);
-        } else {
-          const tasks = await Task.find();
-          req.redisClient.setex('tasks', 3600, JSON.stringify(tasks));
-          res.json(tasks);
-        }
-      });
-    } catch (error) {
-      console.error('Error fetching tasks from MongoDB:', error);
-      res.status(500).json({ error: 'An error occurred' });
-    }
-  };
-exports.addTask = async (req, res) => {
-  try {
-    const task = new Task(req.body);
-    await task.save();
-    req.redisClient.del('tasks');
-    res.sendStatus(201);
-  } catch (error) {
-    console.error('Error adding task:', error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-};
-exports.deleteTask = async (req, res) => {
-  try {
-    await Task.findByIdAndRemove(req.params.id);
-    req.redisClient.del('tasks');
-    res.sendStatus(204);
-  } catch (error) {
-    console.error('Error deleting task:', error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-};
-
-  */
